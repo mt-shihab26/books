a7eab8e96ba8ab0efe7b13f8845210c13d0bd1e6
Shihab Mahamud <shihab4t@gmail.com>
2022-06-19 18:29:24 +0600

    Chapter 04 - loop

---
+++ chapter-04-Loop/README.md
@@ -0,0 +1,6 @@
+## Chapter 4 - Loop
+1. while loop
+2. break statements
+3. continue statements
+4. for loop
+5. nested loop
---
+++ chapter-04-Loop/pg-04.07-namto-of-five-with-for-loop.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+	int n = 5, i;
+
+	for (i = 1; i <= 10; i += 1) {
+		printf("%d x %d = %d\n", n, i, n*i);
+	}
+
+	return 0;
+}
