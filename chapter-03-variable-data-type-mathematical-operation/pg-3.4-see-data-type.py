4e3b10ee8ad233758bc9c83ea5984a58d710987b
Shihab Mahamud <shihab4t@gmail.com>
2022-06-20 00:38:52 +0600

    Chapter 3 - Variable Data type mathematical operation

---
+++ chapter-03-variable-data-type-mathematical-operation/pg-3.4-see-data-type.py
@@ -0,0 +1,8 @@
+# this program one run in interpater
+
+v = 10
+type(v)
+v = '10'
+type(v)
+v = 10.0
+type(v)
---
+++ chapter-03-variable-data-type-mathematical-operation/pg-3.9-welcome.py
@@ -0,0 +1,2 @@
+name = input('What is your name? ')
+print('welcome to Python,', name, "!")
