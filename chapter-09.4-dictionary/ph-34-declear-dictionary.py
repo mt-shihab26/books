e666cf957c2ef62252fe60086ccde4ca9e3e0e7e
Shihab Mahamud <shihab4t@gmail.com>
2022-06-20 00:44:45 +0600

    Chapter 9 - Different data structures of Python

---
+++ chapter-09.4-dictionary/ph-34-declear-dictionary.py
@@ -0,0 +1,11 @@
+
+marks = {1: 77, 2: 76, 5: 62, 4: 78, 3: 65}
+print(type(marks))
+
+print("Marks of roll number 4 is", marks[4])
+
+marks = {1005: 75, 1003: 72, 1002: 70, 1001: 75, 1004: 77}
+print(marks[1003])
+
+marks = {"DH2001": 75, "DH1777": 72, "KH2050": 70}
+print(marks["DH2001"])
---
+++ chapter-09.4-dictionary/ph-37-exer-1-marks-with-dictionary.py
@@ -0,0 +1,13 @@
+marks = {"DH1001": {"Bangla": 74, "English": 73},
+         "DH1002": {"Bangla": 70, "English": 75}}
+
+print(marks["DH1001"])
+
+print(marks["DH1001"]["English"])
+
+marks["DH1003"] = {"Bangla": 68, "English": 72}
+print(marks)
+
+print(marks["DH1003"])
+
+print(marks["DH1003"]["Bangla"])
