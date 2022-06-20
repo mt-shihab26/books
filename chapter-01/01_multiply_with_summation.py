adbf966944bc9be106d83f606ba23ad67c13efd1
Shihab Mahamud <shihab4t@gmail.com>
2022-06-20 10:37:41 +0600

    Chapter 01 - Numbers and calculations

---
+++ chapter-01/01_multiply_with_summation.py
@@ -0,0 +1,9 @@
+n = 100
+m = 99
+
+result = 0
+
+for i in range(m):
+    result = result + n
+
+print(result, n*m);
---
+++ chapter-01/04_dividing_by_0_and_multiplying_by_0.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    int a = 10;
+    int b = 0;
+
+    // Multiplying by 0
+    printf("%d x %d = %d\n", a, b, a * b);
+    printf("%d / %d = %d\n", a, b, a / b);
+}
