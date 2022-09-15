4663df0cb418fa181828a7cac2300fb7db878b87
Shihab Mahamud <shihab4t@gmail.com>
2022-09-15 09:12:20 +0600

    fetchall

--- ch1/fetch/main.go
+++ ch1/fetch/main.go
@@ -5,10 +5,15 @@ import (
 	"io/ioutil"
 	"net/http"
 	"os"
+	"strings"
 )
 
 func main() {
 	for _, url := range os.Args[1:] {
+		if !strings.HasPrefix(url, "http://") {
+			url = "http://" + url
+		}
+		fmt.Println(url)
 		resp, err := http.Get(url)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
@@ -19,6 +24,7 @@ func main() {
 			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
 			os.Exit(1)
 		}
+		fmt.Printf("Status Code: %v", resp.Status)
 		fmt.Printf("%s", b)
 	}
 }
