6adaa3597c51b10506676fcfe743405f623b9045
Shihab Mahamud <shihab4t@gmail.com>
2022-10-01 14:44:18 +0600

    intregating kotlin and rewrite ProductDTO with kotlin

---
+++ eshoppers/src/main/java/com/shihabmahamud/eshoppers/dto/ProductDTO.kt
@@ -0,0 +1,9 @@
+package com.shihabmahamud.eshoppers.dto
+
+import java.math.BigDecimal
+
+class ProductDTO(
+        var name: String,
+        var description: String,
+        var price: BigDecimal
+)
---
+++ eshoppers/src/main/java/com/shihabmahamud/eshoppers/service/FileService.kt
@@ -0,0 +1,23 @@
+package com.shihabmahamud.eshoppers.service
+
+import org.slf4j.LoggerFactory
+import java.io.IOException
+import java.nio.file.Files
+import java.nio.file.Path
+import java.util.function.Consumer
+
+class FileService {
+    fun readFile(path: String?) {
+        LOGGER.debug("reading file from : {}", path)
+        try {
+            Files.readAllLines(path?.let { Path.of(it) })
+                    .forEach(Consumer { line: String? -> LOGGER.info("Line: {}", line) })
+        } catch (e: IOException) {
+            LOGGER.error("Unable to read file from: {}", path, e)
+        }
+    }
+
+    companion object {
+        private val LOGGER = LoggerFactory.getLogger(FileService::class.java)
+    }
+}
