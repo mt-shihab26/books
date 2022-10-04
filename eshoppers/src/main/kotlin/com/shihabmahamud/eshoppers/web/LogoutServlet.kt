a05bfa46195dc9b7f892635de128e926a996c017
Shihab Mahamud <shihab4t@gmail.com>
2022-10-05 02:05:05 +0600

    Logout operation

---
+++ eshoppers/src/main/kotlin/com/shihabmahamud/eshoppers/web/LogoutServlet.kt
@@ -0,0 +1,23 @@
+package com.shihabmahamud.eshoppers.web
+
+import com.shihabmahamud.eshoppers.util.SecurityContext
+import org.slf4j.LoggerFactory
+import java.io.IOException
+import javax.servlet.ServletException
+import javax.servlet.annotation.WebServlet
+import javax.servlet.http.HttpServlet
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
+
+@WebServlet("/logout")
+class LogoutServlet : HttpServlet() {
+    override fun doPost(req: HttpServletRequest, resp: HttpServletResponse) {
+        LOGGER.info("Logging out")
+        SecurityContext.logout(req)
+        resp.sendRedirect("/login?logout=true")
+    }
+
+    companion object {
+        private val LOGGER = LoggerFactory.getLogger(LogoutServlet::class.java)
+    }
+}
