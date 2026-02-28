9a2f1ef07422b3885640efed7b2147587ae93c16
Shihab Mahamud <shihab4t@gmail.com>
2026-02-28 12:34:10 +0600

    guessing game

--- the-rust-book/03_guessing_game/src/main.rs
+++ the-rust-book/03_guessing_game/src/main.rs
@@ -1,7 +1,13 @@
+use rand;
 use std::io;
 
 fn main() {
     println!("Guess the number!");
+
+    let secret_number = rand::random_range(1..=100);
+
+    println!("The secret number is: {secret_number}");
+
     println!("Please input the guess.");
 
     let mut guess = String::new();
