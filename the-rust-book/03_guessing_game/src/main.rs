bca542ccaa2cfcaeb4f9bd77313ba23f001d1ab4
Shihab Mahamud <shihab4t@gmail.com>
2026-02-28 22:31:16 +0600

    guessing game

--- the-rust-book/03_guessing_game/src/main.rs
+++ the-rust-book/03_guessing_game/src/main.rs
@@ -6,23 +6,29 @@ fn main() {
 
     let secret_number = rand::random_range(1..=100);
 
-    println!("The secret number is: {secret_number}");
+    loop {
+        println!("Please input the guess.");
 
-    println!("Please input the guess.");
+        let mut guess = String::new();
 
-    let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+        println!("You guessed: {guess}");
 
-    println!("You guessed: {guess}");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
-    let guess = guess.trim().parse().expect("Please type a number!");
-
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win!"),
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
     }
 }
