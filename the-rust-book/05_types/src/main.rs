467773d06c4d9e2d6be859e4319f7d41badc7e81
Shihab Mahamud <shihab4t@gmail.com>
2026-03-01 23:18:46 +0600

    types

---
+++ the-rust-book/05_types/src/main.rs
@@ -0,0 +1,18 @@
+fn main() {
+    // scalar types
+
+    // integer types
+    let decimal = 98_222;
+    let hex = 0xff;
+    let octal = 0o77;
+    let binary = 0b1111_0000;
+    let byte = b'A';
+
+    println!("decimal = {decimal}");
+    println!("hex = {hex}");
+    println!("octal = {octal}");
+    println!("binary = {binary}");
+    println!("byte = {byte}");
+
+    // floating-point types
+}
