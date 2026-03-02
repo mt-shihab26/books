7ca9f01057475aef79ac0173f1d6fc34025f3887
Shihab Mahamud <shihab4t@gmail.com>
2026-03-03 00:34:03 +0600

    fib

--- the-rust-book/09_problems/src/main.rs
+++ the-rust-book/09_problems/src/main.rs
@@ -1,8 +1,33 @@
-mod problems;
-
-use problems::convert_temperature;
-
 fn main() {
-    let celsius = convert_temperature(98.6);
-    println!("Temperature: {} Celsius", celsius);
+    let celsius = temp(98.6);
+    println!("temperature: {} celsius", celsius);
+
+    for i in 1..11 {
+        println!("{i}th - {} ", fib(i))
+    }
+}
+
+fn temp(fahrenheit: f64) -> f64 {
+    (fahrenheit - 32.0) * (5.0 / 9.0)
+}
+
+fn fib(n: u32) -> u32 {
+    if n == 1 {
+        return 0;
+    }
+
+    if n == 2 {
+        return 1;
+    }
+
+    let mut a = 0;
+    let mut b = 1;
+
+    for _ in 2..n {
+        let t = a;
+        a = b;
+        b = t + b;
+    }
+
+    b
 }
--- the-rust-book/09_problems/src/problems.rs
+++
@@ -1,3 +0,0 @@
-pub fn convert_temperature(fahrenheit: f64) -> f64 {
-    (fahrenheit - 32.0) * (5.0 / 9.0)
-}
