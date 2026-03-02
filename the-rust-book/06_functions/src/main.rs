62c988e94b03d880bd5db6138d62c914814acfa8
Shihab Mahamud <shihab4t@gmail.com>
2026-03-02 08:19:51 +0600

    functions

--- the-rust-book/06_functions/src/main.rs
+++ the-rust-book/06_functions/src/main.rs
@@ -1,9 +1,30 @@
 fn main() {
-    let result = add(5, 10);
+    println!("add = {}", add(5, 10));
 
-    println!("result = {result}");
+    println!("sub = {}", sub(5, 10));
+
+    println!("mul = {}", mul(5, 10));
+
+    println!("div = {}", div(10, 0));
+    println!("div = {}", div(10, 5));
 }
 
 fn add(x: i32, y: i32) -> i32 {
-    return x + y;
+    x + y
+}
+
+fn sub(x: i32, y: i32) -> i32 {
+    x - y
+}
+
+fn mul(x: i32, y: i32) -> i32 {
+    x * y
+}
+
+fn div(x: i32, y: i32) -> i32 {
+    if y == 0 {
+        return 0;
+    }
+
+    x / y
 }
