8e0189ff4940df55ca14d75d8218794cbde9591d
Shihab Mahamud <shihab4t@gmail.com>
2026-03-02 23:16:55 +0600

    loop

--- the-rust-book/08_loops/src/main.rs
+++ the-rust-book/08_loops/src/main.rs
@@ -1,11 +1,23 @@
-use std::{thread::sleep, time::Duration};
+// use std::{thread::sleep, time::Duration};
 
 fn main() {
-    let mut i = 0;
+    // let mut i = 0;
+    //
+    // loop {
+    //     println!("Hello, world! {i}");
+    //     sleep(Duration::from_millis(200));
+    //     i += 1;
+    // }
 
-    loop {
-        println!("Hello, world! {i}");
-        sleep(Duration::from_millis(200));
-        i += 1;
-    }
+    let mut counter = 0;
+
+    let result = loop {
+        counter += 1;
+
+        if counter == 20 {
+            break counter * 2;
+        }
+    };
+
+    println!("The result is {result}");
 }
