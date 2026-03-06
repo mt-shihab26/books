8190b4a798b7df85972194fd3947c2dee9d0c53d
Shihab Mahamud <shihab4t@gmail.com>
2026-03-06 21:48:11 +0600

    ownership

--- the-rust-book/10_ownership/src/main.rs
+++ the-rust-book/10_ownership/src/main.rs
@@ -2,8 +2,9 @@ fn main() {
     let mut s = String::from("hello");
 
     s = takes_ownership(s);
+    let (s, l) = calculate_len(s);
 
-    println!("{s}");
+    println!("{s}, len is: {l}");
 
     let x = 5;
 
@@ -20,4 +21,9 @@ fn takes_ownership(some_string: String) -> String {
 
 fn makes_copy(some_integer: i32) {
     println!("{some_integer}");
+}
+
+fn calculate_len(s: String) -> (String, usize) {
+    let l = s.len();
+    (s, l)
 }
