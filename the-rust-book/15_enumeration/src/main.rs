361d147c369bd9936daa905b57d064bf8a273532
Shihab Mahamud <shihab4t@gmail.com>
2026-03-07 12:49:35 +0600

    run

--- the-rust-book/15_enumeration/src/main.rs
+++ the-rust-book/15_enumeration/src/main.rs
@@ -36,7 +36,7 @@ fn main() {
     println!("{loopback:?}");
 
     let home = IpAddr2::V4(String::from("127.0.0.1"));
-    let loopback = IpAddr2::V4(String::from("::1"));
+    let loopback = IpAddr2::V6(String::from("::1"));
 
     println!("{home:?}");
     println!("{loopback:?}");
