bd6c81cb35fbeac61535b8357a09fff7ac80a336
Shihab Mahamud <shihab4t@gmail.com>
2026-03-07 21:48:17 +0600

    pattern matching

--- the-rust-book/16_pattern_matching/src/main.rs
+++ the-rust-book/16_pattern_matching/src/main.rs
@@ -2,7 +2,6 @@
 enum State {
     Alabama,
     Alaska,
-    // --snip--
 }
 
 enum Coin {
@@ -44,4 +43,24 @@ fn main() {
     println!("5 + 1 = {:?}", plus_one(Some(5)));
     println!("6 + 1 = {:?}", plus_one(Some(6)));
     println!("None + 1 = {:?}", plus_one(None));
+
+    let dice_roll = 9;
+
+    match dice_roll {
+        3 => println!("three"),
+        7 => println!("seven"),
+        other => println!("{other}"),
+    }
+
+    match dice_roll {
+        3 => println!("three"),
+        7 => println!("seven"),
+        _ => println!("other"),
+    }
+
+    match dice_roll {
+        3 => println!("three"),
+        7 => println!("seven"),
+        _ => (),
+    }
 }
