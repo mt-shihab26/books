7e838826c9a31850a366a4af11a2ae19238bb858
Shihab Mahamud <shihab4t@gmail.com>
2026-03-11 05:14:49 +0600

    vector

---
+++ the-rust-book/18_vector/src/main.rs
@@ -0,0 +1,36 @@
+fn main() {
+    let v: Vec<i32> = Vec::new();
+    println!("{v:?}");
+
+    let v = vec![1, 2, 3];
+    println!("{v:?}");
+
+    let mut v = Vec::new();
+
+    v.push(5);
+    v.push(6);
+    v.push(7);
+
+    println!("{v:?}, {}, {}, {}", v[0], v[1], v[2]);
+
+    let second = v.get(2);
+    match second {
+        Some(second) => println!("The second value is {second}"),
+        None => println!("The second value is not exist"),
+    }
+
+    let handard = v.get(100);
+    match handard {
+        Some(handard) => println!("The handard value is {handard}"),
+        None => println!("The handard value is not exist"),
+    }
+
+    for i in &v {
+        println!("{i}");
+    }
+
+    for i in &mut v {
+        *i += 50;
+        println!("{i}");
+    }
+}
