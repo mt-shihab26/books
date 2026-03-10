b1a9ae348d5ad2dd8156803476620404d73173b9
Shihab Mahamud <shihab4t@gmail.com>
2026-03-11 02:26:07 +0600

    module

--- the-rust-book/17_module_system/src/bank/mod.rs
+++ the-rust-book/17_module_system/src/bank/mod.rs
@@ -1,6 +1,14 @@
 pub mod accounts;
 pub mod transactions;
 
+pub mod hello {
+    pub mod world {
+        pub fn foo() {
+            println!("bar");
+        }
+    }
+}
+
 pub fn announce(message: &str) {
     println!("[BANK ANNOUNCEMENT] {message}")
 }
--- the-rust-book/17_module_system/src/main.rs
+++ the-rust-book/17_module_system/src/main.rs
@@ -1,5 +1,24 @@
+use bank::accounts::Account;
+use bank::announce;
+use bank::hello::world::foo;
+use bank::transactions::{deposit, withdraw};
+
 mod bank;
 
 fn main() {
-    println!("Hello, world!");
+    let mut account = Account::new("Bob");
+    println!("Created Account: {account:?}");
+
+    deposit(&mut account, 150);
+    println!("Deposit Account: {account:?}");
+
+    withdraw(&mut account, 50);
+    println!("Withdraw Account: {account:?}");
+
+    announce("Final State");
+    println!("Account: {account:?}");
+
+    println!("The owner name is {}", account.owner);
+
+    foo();
 }
