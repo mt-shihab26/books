50c4806c00ba9ea0d2ecd47ef06de5a7b4b91e98
Shihab Mahamud <shihab4t@gmail.com>
2026-03-10 23:13:48 +0600

    bank

--- the-rust-book/17_module_system/src/bank/mod.rs
+++ the-rust-book/17_module_system/src/bank/mod.rs
@@ -1,2 +1,6 @@
 pub mod accounts;
 pub mod transactions;
+
+pub fn announce(message: &str) {
+    println!("[BANK ANNOUNCEMENT] {message}")
+}
--- the-rust-book/17_module_system/src/bank/transactions.rs
+++ the-rust-book/17_module_system/src/bank/transactions.rs
@@ -1,5 +1,11 @@
 use crate::bank::accounts::Account;
 
-pub fn deposit(account: &mut Account, amount: i32) {}
+pub fn deposit(account: &mut Account, amount: i32) {
+    account.balance += amount;
+}
 
-pub fn withdraw(account: &mut Account, amount: i32) {}
+pub fn withdraw(account: &mut Account, amount: i32) {
+    if account.balance >= amount {
+        account.balance -= amount;
+    }
+}
