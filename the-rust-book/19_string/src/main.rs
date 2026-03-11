358661cdcbb95bc547fb2f38ab6f1c1b12abdd09
Shihab Mahamud <shihab4t@gmail.com>
2026-03-11 17:36:16 +0600

    strings

--- the-rust-book/19_string/src/main.rs
+++ the-rust-book/19_string/src/main.rs
@@ -1,3 +1,29 @@
 fn main() {
-    println!("Hello, world!");
+    let mut s = "foo".to_string();
+
+    s.push_str(" bar");
+
+    println!("{s}");
+
+    let s2 = "buz".to_string();
+
+    let s3 = format!("{s} {s2}"); // format doesn't take ownership
+
+    println!("{s}");
+    println!("{s2}");
+    println!("{s3}");
+
+    let hello = "Здравствуйте";
+
+    let s = &hello[0..4];
+
+    println!("{hello} -> {s}");
+
+    for (i, c) in hello.chars().enumerate() {
+        println!("{i} -> {c}");
+    }
+
+    for (i, b) in hello.bytes().enumerate() {
+        println!("{i} -> {b}");
+    }
 }
