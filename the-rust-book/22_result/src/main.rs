7267a007256ccde632ac62ff9b79064780b543ab
Shihab Mahamud <shihab4t@gmail.com>
2026-03-28 22:29:41 +0600

    wip

--- the-rust-book/22_result/src/main.rs
+++ the-rust-book/22_result/src/main.rs
@@ -1,20 +1,21 @@
-use std::{fs::File, io::ErrorKind};
+use std::fs::File;
+// use std::io::ErrorKind;
 
 fn main() {
     let path = "hello.txt";
 
-    let hello = File::open(&path);
+    let hello = File::open(&path).expect(&format!("{path} should be included in this project"));
 
-    let hello = match hello {
-        Ok(file) => file,
-        Err(error) => match error.kind() {
-            ErrorKind::NotFound => match File::create(&path) {
-                Ok(file) => file,
-                Err(error) => panic!("Problem creating the file: {error:?}"),
-            },
-            _ => panic!("Problem opening the file: {error:?}"),
-        },
-    };
+    // let hello = match hello {
+    //     Ok(file) => file,
+    //     Err(error) => match error.kind() {
+    //         ErrorKind::NotFound => match File::create(&path) {
+    //             Ok(file) => file,
+    //             Err(error) => panic!("Problem creating the file: {error:?}"),
+    //         },
+    //         _ => panic!("Problem opening the file: {error:?}"),
+    //     },
+    // };
 
     println!("{hello:?}");
 }
